02492ec6f504beee6875b3dcf623a11493cf99c2
roeishlomo <asdasda@gmail.com>
2024-03-26 17:46:49 +0200

    random

---
+++ roei.py
@@ -0,0 +1,3 @@
+def print_funny_story():
+    """one time i did a bet with my friend i told him that i can go to random guy and tell him to fight me for no reason for 10 bucks
+    and the random guy actually wanted to fight me so we ended up spliting the money the friend gave me for faking a fight"""
