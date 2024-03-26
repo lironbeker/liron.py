dd3f771062797608435bb838649e0836df1da4cf
the story <tirosh1307@gmail.com>
2024-03-26 18:26:42 +0200

    liron's story + childhood story

--- liron.py
+++ liron.py
@@ -1,1 +1,6 @@
-print("my dog was sitting like a potato and my family and I did a whole scene of him as the victim and all of as trying to eat him :)")
+def print_funny_story():
+    print("my dog was sitting like a potato and my family and I did a whole scene of him as the victim and all of as trying to eat him :)")
+
+
+def print_childhood_story():
+    print("I saw all the sea creatures at the circus  ")
